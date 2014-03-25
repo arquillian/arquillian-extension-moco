13324f5832d39c01d61488ba2a34d9304400f4f1
Aslak Knutsen <aslak@4fs.no>
2014-03-25 13:59:37 +0100

    Add Arquillian Moco API to remote deployment

--- src/main/java/org/jboss/arquillian/moco/client/MocoArchiveAppender.java
+++ src/main/java/org/jboss/arquillian/moco/client/MocoArchiveAppender.java
@@ -3,6 +3,7 @@ package org.jboss.arquillian.moco.client;
 import org.jboss.arquillian.container.test.spi.RemoteLoadableExtension;
 import org.jboss.arquillian.container.test.spi.client.deployment.AuxiliaryArchiveAppender;
 import org.jboss.arquillian.moco.ReflectionHelper;
+import org.jboss.arquillian.moco.api.Moco;
 import org.jboss.arquillian.moco.container.MocoRemoteExtension;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -15,6 +16,7 @@ public class MocoArchiveAppender implements AuxiliaryArchiveAppender {
 		
 		 return ShrinkWrap.create(JavaArchive.class, "arquillian-moco.jar")
 		            .addClass(ReflectionHelper.class)
+		            .addPackage(Moco.class.getPackage())
 		            .addPackage(MocoRemoteExtension.class.getPackage())
 		            .addAsServiceProvider(RemoteLoadableExtension.class, MocoRemoteExtension.class);
 		
